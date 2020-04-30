ca796a12c3ce18e8918520b07aecf10ae2d114c1
mono57 <aymaramono@gmail.com>
2020-04-30 01:24:30 +0100

    add prevent close window

--- views/command_form_view.py
+++ views/command_form_view.py
@@ -1,9 +1,9 @@
-from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QDialog, QMessageBox
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
 from views.layout.CommandFormWindow import Ui_CommandFormWidget
 from views.add_command_article_view import AddCommandArticleView
-
+from db.models import Provider, Command, CommandEntry, Article
 
 class CommandFormView(QDialog, Ui_CommandFormWidget):
     def __init__(self, parent=None):
@@ -14,19 +14,33 @@ class CommandFormView(QDialog, Ui_CommandFormWidget):
         self.tableView.setModel(self.model)
         self.cmd_articles = []
 
+    def compute_price(self):
+        return sum([int(cmd.get('article').selling_price)*int(cmd.get('qte'))
+                    for cmd in self.cmd_articles])
+
+    def closeEvent(self, event):
+        if self.cmd_articles:
+            mBox = QMessageBox.question(
+                self, "Avertissement",
+                "Vous êtes sur le point d'enregistrer une commande. Si vous quittez, les données seront perdues \nVoulez vous continuer ?",
+                QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+
+            if mBox == QMessageBox.Yes:
+                super(CommandFormView, self).closeEvent(event)
+            else:
+                event.ignore()
 
     @pyqtSlot()
     def on_pushButtonAddArticle_clicked(self):
         add_cmd_article_win = AddCommandArticleView()
         add_cmd_article_win.exec_()
         form_data = add_cmd_article_win.get_form_data()
-        item_article = QStandardItem(form_data.get('article_designation'))
-        item_qte = QStandardItem(str(form_data.get('qte')))
-        self.model.setItem(self.model.rowCount(), 0, item_article)
-        self.model.setItem(self.model.rowCount()-1, 1, item_qte)
-        self.cmd_articles.append(form_data)
-
-
-
-
-        
+        if form_data:
+            article = form_data.get('article')
+            item_article = QStandardItem(article.designation)
+            item_qte = QStandardItem(str(form_data.get('qte')))
+            self.model.setItem(self.model.rowCount(), 0, item_article)
+            self.model.setItem(self.model.rowCount()-1, 1, item_qte)
+            self.cmd_articles.append(form_data)
+            total_price = self.compute_price()
+            self.labelTotalCost.setText(str(total_price) + ' F CFA')
