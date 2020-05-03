ff7de3aaac76a419fca56a997885975f541d86fb
mono57 <aymaramono@gmail.com>
2020-05-03 22:27:39 +0100

    update ui

--- views/layout/CommandListWindow.py
+++ views/layout/CommandListWindow.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'command-list.ui'
+# Form implementation generated from reading ui file 'views/ui/command-list.ui'
 #
 # Created by: PyQt5 UI code generator 5.10.1
 #
@@ -9,43 +9,61 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_CommandListWidget(object):
-    def setupUi(self, Form):
-        Form.setObjectName("Form")
-        Form.resize(693, 422)
-        self.verticalLayout_2 = QtWidgets.QVBoxLayout(Form)
+    def setupUi(self, CommandListWidget):
+        CommandListWidget.setObjectName("CommandListWidget")
+        CommandListWidget.resize(693, 422)
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(CommandListWidget)
         self.verticalLayout_2.setObjectName("verticalLayout_2")
         self.verticalLayout = QtWidgets.QVBoxLayout()
         self.verticalLayout.setObjectName("verticalLayout")
-        self.label = QtWidgets.QLabel(Form)
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.label_2 = QtWidgets.QLabel(CommandListWidget)
+        self.label_2.setObjectName("label_2")
+        self.horizontalLayout_2.addWidget(self.label_2)
+        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_2.addItem(spacerItem)
+        self.label = QtWidgets.QLabel(CommandListWidget)
         self.label.setObjectName("label")
-        self.verticalLayout.addWidget(self.label)
-        self.tableView = QtWidgets.QTableView(Form)
+        self.horizontalLayout_2.addWidget(self.label)
+        self.comboBoxFilter = QtWidgets.QComboBox(CommandListWidget)
+        self.comboBoxFilter.setObjectName("comboBoxFilter")
+        self.comboBoxFilter.addItem("")
+        self.comboBoxFilter.addItem("")
+        self.comboBoxFilter.addItem("")
+        self.horizontalLayout_2.addWidget(self.comboBoxFilter)
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+        self.tableView = QtWidgets.QTableView(CommandListWidget)
         self.tableView.setObjectName("tableView")
         self.verticalLayout.addWidget(self.tableView)
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
-        self.label_3 = QtWidgets.QLabel(Form)
+        self.label_3 = QtWidgets.QLabel(CommandListWidget)
         self.label_3.setObjectName("label_3")
         self.horizontalLayout.addWidget(self.label_3)
-        self.label_2 = QtWidgets.QLabel(Form)
-        self.label_2.setObjectName("label_2")
-        self.horizontalLayout.addWidget(self.label_2)
-        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-        self.horizontalLayout.addItem(spacerItem)
-        self.pushButton = QtWidgets.QPushButton(Form)
-        self.pushButton.setObjectName("pushButton")
-        self.horizontalLayout.addWidget(self.pushButton)
+        self.labelTotalCost = QtWidgets.QLabel(CommandListWidget)
+        self.labelTotalCost.setObjectName("labelTotalCost")
+        self.horizontalLayout.addWidget(self.labelTotalCost)
+        spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem1)
+        self.pushButtonQuit = QtWidgets.QPushButton(CommandListWidget)
+        self.pushButtonQuit.setObjectName("pushButtonQuit")
+        self.horizontalLayout.addWidget(self.pushButtonQuit)
         self.verticalLayout.addLayout(self.horizontalLayout)
         self.verticalLayout_2.addLayout(self.verticalLayout)
 
-        self.retranslateUi(Form)
-        QtCore.QMetaObject.connectSlotsByName(Form)
+        self.retranslateUi(CommandListWidget)
+        QtCore.QMetaObject.connectSlotsByName(CommandListWidget)
 
-    def retranslateUi(self, Form):
+    def retranslateUi(self, CommandListWidget):
         _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "Form"))
-        self.label.setText(_translate("Form", "Toutes les commandes"))
-        self.label_3.setText(_translate("Form", "Cout total des commandes :"))
-        self.label_2.setText(_translate("Form", "0,00 F CFA"))
-        self.pushButton.setText(_translate("Form", "Quitter"))
+        CommandListWidget.setWindowTitle(_translate("CommandListWidget", "Form"))
+        self.label_2.setText(_translate("CommandListWidget", "Toutes les commandes"))
+        self.label.setText(_translate("CommandListWidget", "Filtrer par :"))
+        self.comboBoxFilter.setItemText(0, _translate("CommandListWidget", "Toutes les commandes"))
+        self.comboBoxFilter.setItemText(1, _translate("CommandListWidget", "Réceptionnées"))
+        self.comboBoxFilter.setItemText(2, _translate("CommandListWidget", "Non réceptionnées"))
+        self.label_3.setText(_translate("CommandListWidget", "Cout total des commandes :"))
+        self.labelTotalCost.setText(_translate("CommandListWidget", "0,00 F CFA"))
+        self.pushButtonQuit.setText(_translate("CommandListWidget", "Quitter"))
 
